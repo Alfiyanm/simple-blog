36c5b4febbeea6031af343296109fb94e9047a5c
muchlas <muchlas12@gmail.com>
2018-12-13 05:43:27 +0700

    setting Users Table for run Function

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,27 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // reset the users table
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        DB::table('users')->truncate();
+        
+        // generate 3 users / author
+        DB::table('users')->insert([
+            [
+                'name' => "John Doe",
+                'email' => "johndoe@test.com",
+                'password' => bcrypt('secret')
+            ],
+            [
+                'name' => "Peter Parker",
+                'email' => "peterparker@test.com",
+                'password' => bcrypt('secret')
+            ],
+            [
+                'name' => "Edo Masaru",
+                'email' => "edomasaru@test.com",
+                'password' => bcrypt('secret')
+            ],
+        ]);
     }
 }
