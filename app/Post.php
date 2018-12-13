27e5b051d1549771832f0cc49b731ae9831d7224
muchlas <muchlas12@gmail.com>
2018-12-13 10:36:46 +0700

    Display Published post only

--- app/Post.php
+++ app/Post.php
@@ -2,37 +2,44 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $dates = ['published_at'];
+
     public function author()
     {
         return $this->belongsTo(User::class);
     }
-    //
+
     public function getImageUrlAttribute($value)
     {
-        $imageurl = "";
+        $imageUrl = "";
 
         if ( ! is_null($this->image))
         {
             $imagePath = public_path() . "/img/" . $this->image;
-            if (file_exists($imagePath)) $imageurl = asset("/img/" . $this->image);
+            if (file_exists($imagePath)) $imageUrl = asset("img/" . $this->image);
         }
-        return $imageurl;
+
+        return $imageUrl;
     }
 
-    //
     public function getDateAttribute($value)
     {
-        return $this->created_at->diffForHumans();
+        return is_null($this->published_at) ? '' : $this->published_at->diffForHumans();
     }
 
-    //
+
     public function scopeLatestFirst($query)
     {
-        return $this->orderBy('created_at', 'desc');
+        return $query->orderBy('published_at', 'desc');
+    }
+
+    public function scopePublished($query)
+    {
+        return $query->where("published_at", "<=", Carbon::now());
     }
 }
